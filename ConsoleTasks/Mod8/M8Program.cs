175310ca9bc0268ff651cde125f8de404faea7eb
Kristina Kravchenko <kb.krav.perpst@gmail.com>
2022-05-20 12:37:26 +0700

    Add Mod8 runner

---
+++ ConsoleTasks/Mod8/M8Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTasks.Mod8
+{
+    internal static class M8Program
+    {
+        static public void M8Main()
+        {
+            Console.WriteLine("BoopM8");
+        }
+    }
+}
+
---
+++ ConsoleTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mod8.M8Program.M8Main();
+        }
+    }
+}
