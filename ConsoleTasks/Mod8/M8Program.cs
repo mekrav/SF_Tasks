aa2d689e73c4e11ae53a8698d53975cfe76b32ae
Kristina Kravchenko <kb.krav.perpst@gmail.com>
2022-05-20 13:37:23 +0700

    Add GetInfo and GetCatalogs

--- ConsoleTasks/Mod8/M8Program.cs
+++ ConsoleTasks/Mod8/M8Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ConsoleTasks.Mod8
@@ -8,7 +8,46 @@ namespace ConsoleTasks.Mod8
     {
         static public void M8Main()
         {
-            Console.WriteLine("BoopM8");
+            GetCatalogs();
+        }
+
+        static void GetInfo()
+        {
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
+
+        static void GetCatalogs()
+        {
+            string dirName = @"D:\\"; // Прописываем путь к директории Windows
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
         }
     }
 }
