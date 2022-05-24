f59dd4d782bba297d6181d835bc420d8bd0ad4e9
Kristina Kravchenko <kb.krav.perpst@gmail.com>
2022-05-25 00:03:11 +0700

    Final Task3

---
+++ Mod8/Task3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod8
+{
+    internal static class Task3
+    {
+        public static void Run(string FolderPath, int MinNotUsed)
+        {
+            //Не использую try{}catch{}, так как в медодах вида Run уже обрабатываются ошибки
+            Task2.Run(FolderPath);
+            Task1.Run(FolderPath, MinNotUsed);
+            Task2.Run(FolderPath);
+        }
+    }
+}
