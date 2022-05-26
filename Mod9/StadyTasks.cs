249edb2037c9d64bb4995887b45d03902c6d22e9
mekrav <mekrav@gmail.com>
2022-05-26 18:43:52 +0700

    Add Mod9 prolect

---
+++ Mod9/StadyTasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod9
+{
+    internal static class StadyTasks
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
