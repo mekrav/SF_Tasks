44eba1ebd99bb371b68b597f3e926c9452b559e5
mekrav <mekrav@gmail.com>
2022-06-13 15:09:00 +0700

    Mod 12

---
+++ Mod12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Mod12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = User.ParseArgs(args);
+            // Начало
+            foreach(var user in users)
+            {
+                Console.WriteLine("Здравствуйте, " + user.Name);
+                if (!user.IsPremium)
+                {
+                    ShowAds();
+                }
+            }
+            //Конец
+
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Mod12/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod12
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        User (string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+        public static User[] ParseArgs(string[] data)
+        {
+            //Просто заглушка
+            return null;
+        }
+    }
+}
