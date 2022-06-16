5a4f6ea655f41c2d54508cefe81dbac708c67d7a
mekrav <mekrav@gmail.com>
2022-06-17 01:10:55 +0700

    Mod 13 study task

---
+++ Mod13/Part1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod13
+{
+    internal static class Part1
+    {
+        public static int WordCountTxt(string path)
+        {
+            string data;
+            if (File.Exists(path))
+            {
+                data = File.ReadAllText(path);
+                char[] delimeters = { ' ', '\n', '\r' };
+                string[] words = data.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                return words.Length;
+            }
+
+            return 0;
+        }
+    }
+}
--- Mod13/Program.cs
+++ Mod13/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mod13
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            /* //Part1
+            string path = "D:\\SkillFactory\\cdev_Text.txt";
+            Console.WriteLine(Part1.WordCountTxt(path));
+            */
+            /*//Part3
+            string data = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+            var characters = data.ToCharArray();
+
+            var symbols = new HashSet<char>();
+
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+
+            Console.WriteLine(symbols.Count);
+            */
         }
     }
 }
