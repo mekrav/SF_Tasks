5d3c47e4e1fea21fc74a8e6b65575cc51cf44a67
mekrav <mekrav@gmail.com>
2022-06-23 02:35:05 +0700

    Mod 16. Final task

---
+++ Mod16.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace Mod16.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(calculator.Additional(10, 20), 30);
+        }
+
+        [Test]
+        public void SybtructionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(calculator.Subtraction(5, 2), 3);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(calculator.Miltiplication(10, 20), 200);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(10, calculator.Division(100, 10));
+        }
+
+        [Test]
+        public void DivisionMustThrow()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(100, 0));
+        }
+    }
+}
---
+++ Mod16/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod16
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
