1ef36111b83119b40879ac734730d108c4aa31b4
mekrav <mekrav@gmail.com>
2022-06-23 21:28:45 +0700

    Mod17. Before refactoring

---
+++ Mod17/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod17
+{
+    internal class Calculator
+    {
+        // Метод для расчета процентной ставки
+        public static void CalculateInterest(Account account)
+        {
+            if (account.Type == "Обычный")
+            {
+                // расчет процентной ставки обычного аккаунта по правилам банка
+                account.Interest = account.Balance * 0.4;
+
+                if (account.Balance < 1000)
+                    account.Interest -= account.Balance * 0.2;
+
+                if (account.Balance < 50000)
+                    account.Interest -= account.Balance * 0.4;
+            }
+            else if (account.Type == "Зарплатный")
+            {
+                // расчет процентной ставк зарплатного аккаунта по правилам банка
+                account.Interest = account.Balance * 0.5;
+            }
+        }
+    }
+}
